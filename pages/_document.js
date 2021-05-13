8c71231d4f5f46007a037aa4f37115e6ef0f3958
Yrobot <Yrobot@qq.com>
2021-05-13 20:00:42 +0800

    添加GA埋点

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,15 @@ class MyDocument extends Document {
             crossOrigin='anonymous'
           />
           <link rel='stylesheet' href='//at.alicdn.com/t/font_2533274_yhzuq4j0hfr.css' />
+          <script async src='https://www.googletagmanager.com/gtag/js?id=G-1DXREC8Y53'></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}
+              gtag('js', new Date()); gtag('config', 'G-1DXREC8Y53');
+            `,
+            }}
+          />
         </Head>
         <body>
           <Main />
