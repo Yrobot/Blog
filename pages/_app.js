a54cacf80841e57c275c3206af093da0a6b97166
Yrobot <Yrobot@qq.com>
2021-11-27 21:25:54 +0800

    添加simple analytic

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,24 @@
-import "tailwindcss/tailwind.css"
+import React from "react";
+import Script from "next/script";
+
+import "tailwindcss/tailwind.css";
 import "./global.sass";
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <React.Fragment>
+      <Component {...pageProps} />
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+      <noscript>
+        {/* eslint-disable @next/next/no-img-element */}
+        <img
+          src="https://queue.simpleanalyticscdn.com/noscript.gif"
+          alt=""
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </noscript>
+    </React.Fragment>
+  );
 }
+
+export default MyApp;
