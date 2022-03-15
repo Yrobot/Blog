18654dfab5c35121699729ecbd33fd4da639695c
Yrobot <yrobot@qq.com>
2022-03-15 17:04:34 +0800

    修复完成blog数量

--- components/BlogProgress/index.js
+++ components/BlogProgress/index.js
@@ -1,11 +1,11 @@
 import { Trans } from "I18N";
 
-export default function BlogProgress({ compeled = 0, progress = 0 }) {
+export default function BlogProgress({ completed = 0, progress = 0 }) {
   return (
     <div className="helvetica-neue mb-space flex flex-row items-start justify-between">
       <div className="card mr-space flex w-0 flex-auto flex-row items-center justify-center py-space">
         <div className="mr-space flex-none text-3xl md:mr-space md:text-5xl xl:text-6xl">
-          {compeled}
+          {completed}
         </div>
         <div className="flex-none whitespace-pre text-sm md:text-base xl:text-xl">
           <Trans>{`Blogs\ncompleted`}</Trans>
